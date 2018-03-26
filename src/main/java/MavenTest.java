bf14e6f4a6d49053c939b400712235eb357299b5
Chris Kelley <kelleyc718@gmail.com>
2018-03-26 11:35:18 -0500

    Fixed method error

--- src/main/java/MavenTest.java
+++ src/main/java/MavenTest.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class MavenTest {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Something: ");
         String userInput = sc.nextLine();
 
-        if (!StringUtils.isAlphaSpace(userInput)) {
+        if (!StringUtils.isNumeric(userInput)) {
             System.out.println("There are numbers in this string.");
         } else {
             System.out.println("You entered: " + userInput);
@@ -17,6 +18,19 @@ public class MavenTest {
 
             String reverseStr = StringUtils.reverse(userInput);
             System.out.println(reverseStr);
+
+            MavenTest.output(userInput);
+        }
+    }
+
+    private static void output(String userInput) {
+
+        boolean vowelCheck = (StringUtils.containsAny("aeiou", userInput));
+
+        if (vowelCheck) {
+            System.out.println("Contains vowels");
+        } else {
+            System.out.println("Does not contain vowels");
         }
     }
 }
